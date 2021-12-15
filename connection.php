e45163d2b52d09ec2f27c33eba482302c08ab32d
Rupesh <rupeshpatil9007@gmail.com>
2021-12-15 22:42:06 +0530

    init2

--- connection.php
+++ connection.php
@@ -18,11 +18,9 @@ else{
     echo "Connection Success!<br><br>";
 }
 
-// $water_level = $_GET["water_level"];
-// $motor_status = $_GET["motor_status"]; 
+$water_level = $_GET["water_level"];
+$motor_status = $_GET["motor_status"]; 
 
-$water_level=15;
-$motor_status=true;
 $query = "UPDATE motor_status SET status ='$motor_status' where motor_id=2021";
 $result1 = mysqli_query($connect,$query);
 $query = "UPDATE water_level SET water_level ='$water_level' where tank_id=2021";
